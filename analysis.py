75eb8f1d7ebff3bf6bf5ee4929eb058834c6360b
orestispanago <orestispanago@gmail.com>
2021-04-25 16:39:32 +0300

    Added time in DataArray, used for loop,  saving to many or one nc file

--- analysis.py
+++ analysis.py
@@ -6,40 +6,39 @@ import glob
 import geopandas
 import rioxarray
 from shapely.geometry import mapping
+from numpy import newaxis
 
 hdf_files = glob.glob("modis/*.hdf")
-
-# ss = xr.open_dataset(hdf_files[0])
-# rds = rioxarray.open_rasterio("raw/MOD13C2.A2010001.006.2015198205120.hdf")
-
-# var = ss.variables
-# yyd = hdf_files[0].split("/")[1].split(".")[1][1:]
-
-# year_day = datetime.datetime.strptime(yyd, "%Y%j")
-
-
-ds = xr.open_dataset('modis/MOD13C2.A2010001.006.2015198205120.hdf')
-
-ndvi = ds['CMG 0.05 Deg Monthly NDVI']/100000000
-
+shp = geopandas.read_file('shapefiles/GRC_adm3.shp')
 
 #generate the Lat and Lon for 0.1x0.1 
 lats = np.linspace(90, -90, 3600)
 lats = np.round(lats, 2)
 lons = np.linspace(-180, 180, 7200)
 lons = np.round(lons, 2)
+    
+def get_datetime(base_name):
+    yyd = base_name.split(".")[1][1:]
+    return datetime.datetime.strptime(yyd, "%Y%j")
 
+for hdf_file in hdf_files:   
+    base_name = os.path.basename(hdf_file)
+    ds = xr.open_dataset(hdf_file)
+    ndvi = ds['CMG 0.05 Deg Monthly NDVI']/100000000
+    
+    date_dim = get_datetime(base_name)
+    values = ndvi.values[:,:, newaxis]
+    
+    dsa = xr.DataArray(values,
+                       name="NDVI", 
+                       dims=["y","x",'time'], 
+                       coords=[lats, lons, [date_dim]])
+    
+    dsa = dsa.rio.set_crs(4326)
+    clipped = dsa.rio.clip(shp.geometry.apply(mapping), shp.crs)
+    clipped.to_netcdf(f'clipped/{base_name[:-4]}.nc')
 
-dsa = xr.DataArray(ndvi.values, 
-                   name="NDVI", 
-                   dims=["y","x"], 
-                   coords=[lats,lons])
-# dsa.plot()
-
-
-
-shp = geopandas.read_file('shapefiles/GRC_adm3.shp')
-
-dsa = dsa.rio.set_crs(4326)
-clipped = dsa.rio.clip(shp.geometry.apply(mapping), shp.crs)
-clipped.plot(vmin=0)
+filenames = glob.glob('clipped/*.nc')
+ds_all = xr.open_mfdataset(filenames, combine='by_coords')
+ds_all = ds_all.load()
+ds_all.to_netcdf('clipped/all_years.nc')
